9cbe7597b609c147dfd0b02ab1b633db429deda2
Vlad.d electronics <86973932+vxz00@users.noreply.github.com>
2023-08-09 22:46:27 -0600

    Add files via upload

---
+++ _1_SERVO_MOTOR_DIRECTO.ino
@@ -0,0 +1,42 @@
+//VLAD .D ELECTROICS
+#include <Servo.h> 
+
+Servo myservo;
+
+
+void setup()
+{
+ myservo.attach(9);    
+
+}
+   void loop() 
+{ 
+   
+myservo.write(0);  //ready
+       delay(950);; 
+   
+myservo.write(45);  //ready
+       delay(950);; 
+           
+myservo.write(91);  //ready
+       delay(950);; 
+       
+myservo.write(120);  //ready
+       delay(950);; 
+          
+myservo.write(180);  //ready
+       delay(950);; 
+       
+myservo.write(120);  //ready
+       delay(950);;    
+       
+myservo.write(91);  //ready
+      delay(950);;   
+       
+myservo.write(45);  //ready
+       delay(950);;      
+       
+       }
+    
+       
+
