2b9ded151468bd1cd7bb5ee2bf36a6eba2d0079f
Vlad.d electronics <86973932+vxz00@users.noreply.github.com>
2023-08-09 23:07:30 -0600

    Add files via upload

---
+++ _7_SERVO_MOTOR__SERIAL__LETRAS_/_7_SERVO_MOTOR__SERIAL__LETRAS_.ino
@@ -0,0 +1,56 @@
+#include <Servo.h> 
+
+Servo servo1;
+Servo servo2;
+
+
+
+int val;
+
+void setup()
+{
+servo1.attach(9);
+servo2.attach(10);
+servo1.write(90);  //ready
+servo2.write(90);  //ready
+       delay(950); 
+       
+Serial.begin(9600);
+
+}
+void loop()
+{
+if (Serial.available()) {
+val = Serial.read();
+
+
+if (val == 'A') {
+
+servo1.write(10);  //1
+       delay(950);; 
+}
+if (val == 'S') {
+
+servo1.write(170);  //1S
+       delay(950);; 
+ }     
+
+if (val == 'L') {
+
+servo2.write(175);  //2
+       delay(950);; 
+}
+if (val == 'K') {
+
+servo2.write(12);  //2
+       delay(950);;        
+       
+}
+}
+}
+
+
+
+
+
+
