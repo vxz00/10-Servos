44251218cfeb00f7528254a3e7a9e36672676f49
Vlad.d electronics <86973932+vxz00@users.noreply.github.com>
2023-08-09 23:02:26 -0600

    Add files via upload

---
+++ _4_SERVO_MOTOR__POTENCIOMETRO/_4_SERVO_MOTOR__POTENCIOMETRO.ino
@@ -0,0 +1,21 @@
+// Controlling a servo position using a potentiometer (variable resistor) 
+// by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
+
+#include <Servo.h> 
+ 
+Servo myservo;  
+ 
+int potpin = 0;  // analog pin   potenciometro
+int val;    
+void setup() 
+{ 
+  myservo.attach(9);   //salida del servo
+} 
+ 
+void loop() 
+{ 
+  val = analogRead(potpin);             
+  val = map(val, 0, 1023, 0, 179);      
+  myservo.write(val);                    
+  delay(15);                              
+} 
